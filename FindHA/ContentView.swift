fa21afc77a2d57fcf0e5c73891316f383502fa14
jaminleee <jamin_00@naver.com>
2023-12-20 00:32:17 +0900

    [Add] add proj
    git status

---
+++ FindHA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindHA
+//
+//  Created by 이자민 on 12/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindHA/FindHAApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindHAApp.swift
+//  FindHA
+//
+//  Created by 이자민 on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FindHAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
